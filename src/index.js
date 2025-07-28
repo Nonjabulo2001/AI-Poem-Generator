ed0dee3dceaf096e56c4a5151c1e5ef39726c2bd
Nonjabulo Chonco <nonjabulo918@gmail.com>
2025-07-28 08:52:50 -0700

    added more guideline

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ function generatePoem(event) {
     event.preventDefault();
     let instructions = document.querySelector("#instructions");
     let apiKey = "4df1bdctd7b970a1c5dof432b8f97964";
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    let context = "You are a romantic Poem expect and love to generate short poem in basic HTMl. Make sure to follow a user instructions";
+     let context = "You are a romantic Poem expect and love to generate short poem in basic, seperate line with <br>.Your aim is to generate 2 to 4 lines. Make sure to follow a user instructions.Donot give xplaination about html and html word";
     let prompt = ` User instructions: Generate a French poem ${instructions.value}`;
+
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
 
     console.log("Generating a poem");
     console.log(`prompt:${prompt}`);
